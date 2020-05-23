6c15a01aa726ff4a590150cbc548486fe048ea48
nghianguyen <nghiatan1234@gmail.com>
2020-05-23 15:00:26 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -2,22 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Image(props) {
+  return (
+    <div>
+      <img src={props.imageURL}/>
+    </div>
+  )
+}
+
+function Header(props) {
+  return (
+    <div>
+      <p>{props.title}</p>
+      <h3>{props.detail}</h3>
+    </div>
+  )
+}
+
+
+
+function Price(props) {
+  return (
+    <div>
+      <span> {props.basePrice} </span>
+      <span style={{ textDecoration: 'line-through' }}>{props.discountPrice}</span>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image imageURL="https://media3.scdn.vn/img4/2020/03_18/fx0jRvqJ7wVYmcj9swIb_simg_b5529c_250x250_maxb.jpg"/>
+        <Header title="FURNITURE" detail="Minimal Deco Furniture"/>
+        <Price basePrice="$119.000 USD" discountPrice="$230.0000 USD"/>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello <code>World!</code>
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
